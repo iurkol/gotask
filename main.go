c5d25bf40cbf0f6bb09b8384261cd95ea8c0397a
Iurii Kolochkov <iuriikolochkov@gmail.com>
2018-06-01 09:37:46 +0300

    Assignment 3

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -9,28 +12,39 @@ import (
 )
 
 const (
-	LOGFILE    = "/tmp/requests.txt"
-	SEPLINE    = "------------"
-	LOGRECORD  = "IP: %s\nHostname: %s\nPath: %s\nTimestamp: %s\n%s\n"
 	TIMELAYOUT = "2006-01-02 15:04:05 +0000"
+
+	INSERTQUERY = `INSERT INTO requests (ip, path, host, requested_at)
+ VALUES ($1, $2, $3, $4);`
 )
 
-func main() {
-	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
-	logfile, err := os.OpenFile(LOGFILE, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
-	if err != nil {
-		panic("Can't open log file.")
-	}
-	defer logfile.Close()
-	log.SetOutput(logfile)
+var Connection *sqlx.DB
+var err error
 
+func init() {
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbPasswd := os.Getenv("POSTGRES_PASSWORD")
+	dbHost := os.Getenv("POSTGRES_HOST")
+
+	postgresDataSource := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable",
+		dbUser, dbName, dbPasswd, dbHost)
+
+	Connection, err = sqlx.Connect("postgres", postgresDataSource)
+	if err != nil {
+		log.Panicln("Error connecting to db:", err)
+	}
+}
+
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf(LOGRECORD,
-			strings.Split(r.RemoteAddr, ":")[0],
-			strings.Split(r.Host, ":")[0],
-			r.URL.Path,
-			time.Now().Format(TIMELAYOUT),
-			SEPLINE)
+		timeStamp := time.Now().Format(TIMELAYOUT)
+		ip := strings.Split(r.RemoteAddr, ":")[0]
+		host := strings.Split(r.Host, ":")[0]
+		path := r.URL.Path
+
+		Connection.MustExec(INSERTQUERY, ip, path, host, timeStamp)
+
 		w.Write([]byte("Hello World!"))
 	})
 
